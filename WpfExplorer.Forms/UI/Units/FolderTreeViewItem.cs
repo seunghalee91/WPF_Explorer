db79372a1a0b9d4bc0bf6b5c96cf883d9e6a5d85
이승하 <seungha.lee@mirero.co.kr>
2024-12-16 15:01:21 +0900

    refact : TreeView,TreeViewItem 추가

---
+++ WpfExplorer.Forms/UI/Units/FolderTreeViewItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Forms.UI.Units
+{
+    public class FolderTreeViewItem : TreeViewItem
+    {
+        static FolderTreeViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FolderTreeViewItem), new FrameworkPropertyMetadata(typeof(FolderTreeViewItem)));
+        }
+    }
+}
