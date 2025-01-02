36bb9caf5e0529de8ff2fc7b459fd693b6689c19
이승하 <seungha.lee@mirero.co.kr>
2025-01-02 14:50:39 +0900

    feat : directory 조회위한 class 추가

--- WpfExplorer.Forms/Local/ViewModels/ExplorerWindowViewModel.cs
+++ WpfExplorer.Forms/Local/ViewModels/ExplorerWindowViewModel.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using WpfExplorer.Support.Local.Helpers;
 
 namespace WpfExplorer.Forms.Local.ViewModels
 {
     public class ExplorerWindowViewModel
     {
-        //public string TESTMessage{get;set;}
-
-        public ExplorerWindowViewModel()
+        public ExplorerWindowViewModel(DirectoryManager directoryManager)   //의존성 주입
         {
-            //TESTMessage = "Explorer view model";
+            MessageBox.Show(directoryManager.DownloadDirectory);    //directory 조회 확인
         }
     }
 }
---
+++ WpfExplorer.Support/Local/Models/FileInfoBase.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Jamesnet.Wpf.Controls;
+
+namespace WpfExplorer.Support.Local
+{
+    internal partial class FileInfoBase : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _isDenied;
+    
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public int Depth { get; set; }
+        public long Length { get; set; }
+        public IconType IconType { get; set; }
+
+        public void test()
+        { 
+            IsDenied = true;    //IsDenied를 생성하지 않았지만 [ObservableProperty]어트리뷰트 사용으로 자동으로 생성된 것.
+        }
+    }
+}
